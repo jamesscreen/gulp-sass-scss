89ec5ae0dcf7841e82b4fecf56ed835045f3687d
jamesscreen <jamesscreen@yandex.ru>
2019-02-07 23:10:29 +0300

    Подключен jQuery. Добавлена смена цветов на левой панели: отрпаботана активная ссылка, подсветка крайних от активной ссылок.

--- script/main.js
+++ script/main.js
@@ -1,5 +1,17 @@
-$(function(){
+$(function () {
   
-  $('.links')
-  
+  $('.nav__item').click(function(){
+    
+    $('.nav__item').removeClass('active');
+    $('.nav__item').css('background-color', '#191c24');
+    $('.nav__item a').css('color', '#fff');
+    $('.nav__item:last-child a').css('color', '#808592');
+    
+    $(this).addClass('active').css('background-color', '#2a2d37');
+    $(this).children('a').css('color', '#808592');
+    $(this).prev().css('background-color', '#1f2229');
+    $(this).next().css('background-color', '#1f2229');
+    
+  });
+
 });
